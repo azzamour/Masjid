990d8beded4741201f1f9c91374dca5971d8a41e
arianidwiwulandari <ariani_dwiwulandari@yahoo.com>
2017-10-24 21:20:17 +0700

    nambah testing sign out

--- app/src/androidTest/java/com/kleinkarasu/masjid/LoginInsTest.java
+++ app/src/androidTest/java/com/kleinkarasu/masjid/LoginInsTest.java
@@ -132,7 +132,10 @@ public class LoginInsTest {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
+        intended(hasComponent(WargaActivity.class.getName()));
+        FirebaseAuth firebaseAuth = FirebaseAuth.getInstance();
+        openActionBarOverflowOrOptionsMenu(getInstrumentation().getTargetContext());
+        onView(withText("Log out")).perform(click());
         //firebaseAuth.signOut();
     }
 }
